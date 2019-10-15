28c3b8e6a775e15ea22437bc1757bdc11899ed91
Yasu <yasu@pixiv.co.jp>
2019-10-16 06:26:23 +0900

    Makefileを共通化した

---
+++ apps/hoge/go.mod
@@ -0,0 +1,3 @@
+module github.com/tamanobi/gitops-sample-app/apps/fuga
+
+go 1.13
--- apps/hoge/main.go
+++ apps/hoge/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Fuga!")
+		fmt.Fprintf(w, "Hello Hoge!")
 	})
 	log.Fatal(http.ListenAndServe(":1333", nil))
 }
